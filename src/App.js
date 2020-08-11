f196f0700405dba894a1adfd0917be3f03a6bbcb
jordanrob709 <jordanrob709@gmail.com>
2020-08-11 18:01:35 +0300

    added input, button and input & messages state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ input, setInput ] = useState('');
+  const [ messages, setMessages ] = useState([]);
+
+  console.log(input)
+  console.log(messages)
+
+  const sendMessage = (event) => {
+    event.preventDefault()
+    setMessages([...messages, input])
+    setInput('')
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <form>
+        <input value={ input } onChange={ event => setInput(event.target.value) } ></input>
+        <button type='submit' onClick={ sendMessage } >Send</button> 
+      </form>
+    
+
+      {
+        messages.map(message => (
+        <p>{message}</p>
+        ))
+      }
+
+
     </div>
   );
 }
